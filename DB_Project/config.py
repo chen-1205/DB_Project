8de527687989470bdad48a87885154977ac513ad
chen-1205 <62638423+chen-1205@users.noreply.github.com>
2025-01-12 23:29:58 +0800

    Update config.py

--- DB_Project/config.py
+++ DB_Project/config.py
@@ -1,10 +1,10 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY", "default_secret_key")  # 從環境變量加載，默認值為 "default_secret_key"
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.abspath('db_project.db')}"  # SQLite 文件路徑
+    SECRET_KEY = os.environ.get("SECRET_KEY", "default_secret_key")  
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.abspath('db_project.db')}"  
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    UPLOAD_FOLDER = 'static/images'  # 指定上傳目錄
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 最大上傳文件大小為 16 MB
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  # 支持的文件格式
+    UPLOAD_FOLDER = 'static/images'  
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}  
